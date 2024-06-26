d3bdfe768602bc5aa631ef21edcbb3643d045f25
kiobc <brayan.calderon3464@utc.edu.ec>
2024-06-26 14:56:47 -0500

    desarrollo del formulario Login v1

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Login } from '../../Interfaces/login'; 
+
+import { UsuarioService } from '../../Services/usuario.service';
+import { UtilidadService } from '../../Reutilizable/utilidad.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'] 
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  formularioLogin: FormGroup;
+  ocultarPassword = true;
+  mostrarLoading = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private _usuarioServicio: UsuarioService,
+    private _utilidadServicio: UtilidadService
+  ) {
+    this.formularioLogin = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  iniciarSesion(): void {
+    this.mostrarLoading = true;
+
+    const request: Login = {
+      correo: this.formularioLogin.value.email,
+      clave: this.formularioLogin.value.password
+    };
+
+    this._usuarioServicio.iniciarSesion(request).subscribe({
+      next: (data) => {
+        if (data.status) {
+          this._utilidadServicio.guardarSesionUsuario(data.value);
+          this.router.navigate(['/pages']);
+        } else {
+          this._utilidadServicio.mostrarAlerta("No se encontraron coincidencias", "Oops!");
+        }
+      },
+      complete: () => {
+        this.mostrarLoading = false;
+      },
+      error: () => {
+        this._utilidadServicio.mostrarAlerta("Ocurrió un error", "Oops!");
+        this.mostrarLoading = false; 
+      }
+    });
+  }
 }
